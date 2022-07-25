4351de445e8143a13c6871af8b45073b0a283146
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-07-25 20:46:45 +0530

    edit more

--- game.js
+++ game.js
@@ -14,17 +14,36 @@ function turn(){
 }
 function roll_dice(){
     var button = document.getElementById('roll');
+    if(button.innerHTML == "Rolling"){ //do not continue while rolling
+        return;
+    }
     button.innerHTML = "Rolling";
     button.style.backgroundColor = "darkgreen";
-    var dice1 = document.getElementById('dice_1');
-    var dice2 = document.getElementById('dice_2');
-
+    
+    var dice1_val, dice2_val;
+    dice1_val = generate_num();
+    dice2_val = generate_num();
+    setInterval(random_image(), 1000);
 }
+
+function generate_num(){
+    var rand = Math.random();
+    var num = Math.floor(rand * 6) + 1;
+    return num;
+}
+
 let dice_set = new Array(); //to store the dice images
 
 for(i = 0; i < 6; i++){ //storing the images
     dice_set[i] = new Image();
-    dice_set[i].src = './Dice_Images/dice_'+toString(i+1)+'.png';
+    dice_set[i].src = './Dice_Images/dice_'+(i+1)+'.png';
 }
 
+function random_image(){
+    var dice1 = document.getElementById("dice_1");
+    var dice2 = document.getElementById("dice_2");
+    dice1.src = "./Dice_Images/dice_3.png";
+    dice2 = dice_set[generate_num()-1];
+    console.log(dice1);
+}
 
