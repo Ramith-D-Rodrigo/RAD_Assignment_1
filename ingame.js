b9bbebec6255212f07d251c437681378de2b5a28
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-07-26 11:39:49 +0530

    rename game.js to ingame.js

--- ingame.js
+++ ingame.js
@@ -1,8 +1,3 @@
-function play_button(){
-    var btn = document.getElementById('play_button');
-    btn.style.backgroundColor = "darkgreen";
-    setTimeout(() => {  window.open("./ingame.html","_self"); }, 500); //start the game after 1second
-}
 
 let turn_val = 1;   //starting turn (player 1 -> turn = 1 and player 2 -> turn = 2)
 
@@ -26,7 +21,7 @@ function random_image(){    //getting random images for the dices
     dice2.src = dice_set[generate_num()-1];
 }
 
-const roll_button = document.getElementById('roll');
+const roll_button = document.getElementById("roll");
 
 
 roll_button.addEventListener("click", function(){
@@ -56,7 +51,7 @@ roll_button.addEventListener("click", function(){
     dice2_val = generate_num(); //player's rolled dice 2 value
     console.log(dice1_val, dice2_val);
 
-    var dice_shuffle = setInterval("random_image()", 50); //start rolling animation
+    var dice_shuffle = setInterval("random_image()", 75); //start rolling animation
 
     setTimeout(() => {  //end rolling animation and find the rolled dices
         if(dice1_val == 1 && dice2_val == 1){   //getting 1 on both dices
@@ -73,36 +68,41 @@ roll_button.addEventListener("click", function(){
         this.innerHTML = "Roll Dice"; //revert back
         this.style.backgroundColor = "green";
         clearInterval(dice_shuffle);
+
         var dice1 = document.getElementById("dice_1");
         var dice2 = document.getElementById("dice_2");
-        dice1.src = dice_set[dice1_val-1];  //actual rolled dices
+
+        dice1.src = dice_set[dice1_val-1];  //actual rolled dices' images
         dice2.src = dice_set[dice2_val-1];
+
         if(parseInt(score.innerHTML) >= 100){ //if one player has reached 100 score
             var notification = document.getElementById("result");
             notification.style.display = "block";
             var winner = document.getElementById('winner');
-            if(turn == 1){
-                winner.innerHTML =  "Winner is Player 1";
-            }
-            else{
-                winner.innerHTML =  "Winner is Player 2";
-            }
+            winner.innerHTML = "Winner is Player " + turn;  //print the winner
             this.disabled = true;   //cannot continue anymore
         }
-    },3000);
+    },2000);
 
 });
 
 
 let dice_set = new Array(); //to store the dice images
 
-for(i = 0; i < 6; i++){ //storing the images
+for(i = 0; i < 6; i++){ //storing the images' reference links
     dice_set[i] = './Dice_Images/dice_'+(i+1)+'.png';
 }
 
 
-const try_again_btn = document.getElementById("tryagain");
+const try_again_btn = document.getElementById("tryagain");  //try again event
 try_again_btn.addEventListener("click", function(){ //try again
     window.location.reload();   
+});
+
+const close_btn = document.getElementById("close");
+
+close_btn.addEventListener("click", function(){ //close button event
+    var msg_box = this.parentNode.parentNode.parentNode;
+    msg_box.style.display = "none";
 });
 
